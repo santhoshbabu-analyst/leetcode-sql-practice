41f815f10604fbd25e91fbc1a9b421ced397d6a3
Santhosh babu <santhoshbabus.analyst@gmail.com>
2026-02-14 21:04:53 +0530

    Time: 605 ms (56.21%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,10 +1,14 @@
-# Write your MySQL query statement below
-WITH CTE AS(SELECT *, MIN(order_date)first_order
+WITH cte as
+(
+SELECT 
+    customer_id,
+    MIN(order_date)first_ordr
 FROM Delivery 
 GROUP BY customer_id)
 
-SELECT ROUND(SUM(CASE WHEN d.order_date = d.customer_pref_delivery_date THEN 1 ELSE 0 END)*100.0/COUNT(*),2)immediate_percentage 
-FROM CTE c
+SELECT 
+    ROUND(SUM(CASE WHEN c.first_ordr = d.customer_pref_delivery_date THEN 1 ELSE 0 END)*100.0/COUNT(*),2)immediate_percentage 
+FROM cte c
 JOIN Delivery d
 ON c.customer_id = d.customer_id 
-AND c.first_order = d.order_date 
+AND c.first_ordr = d.order_date 
