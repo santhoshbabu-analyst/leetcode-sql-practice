d9977743b497af48968632cee50871eaca9a712b
Santhosh babu <santhoshbabus.analyst@gmail.com>
2026-02-14 18:22:19 +0530

    Time: 335 ms (75.97%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,4 +1,6 @@
-SELECT query_name,ROUND(AVG(rating/position),2)quality,
-ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)*100.0/COUNT(*),2)poor_query_percentage 
+SELECT 
+    query_name,
+    ROUND(AVG(rating/position),2)quality,
+    ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)*100.0/COUNT(*),2)poor_query_percentage 
 FROM Queries 
 GROUP BY query_name;
