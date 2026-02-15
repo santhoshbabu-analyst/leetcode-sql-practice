35df07c11d72d0cf9e7105ee484c7a5ae696ec00
Santhosh babu <santhoshbabus.analyst@gmail.com>
2026-02-15 17:43:06 +0530

    Time: 760 ms (31.29%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,12 +1,18 @@
-WITH cte AS (
-    SELECT *,
-           DENSE_RANK() OVER (PARTITION BY product_id ORDER BY year) AS rn
-    FROM Sales
+WITH cte as
+    (
+SELECT 
+    product_id,
+    year,
+    quantity,
+    price,
+    DENSE_RANK() OVER(PARTITION BY product_id ORDER BY year)rnk
+FROM Sales 
 )
 
-SELECT product_id,
-       year AS first_year,
-       quantity,
-       price
+SELECT 
+    product_id,
+    year first_year,
+    quantity,
+    price 
 FROM cte
-WHERE rn = 1;
+WHERE rnk = 1;
