8c07f8037184245a3248e43de622f205fc1ca53f
Santhosh babu <santhoshbabus.analyst@gmail.com>
2026-05-26 13:01:20 +0530

    Time: 284 ms (76.9%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,6 +1,6 @@
-SELECT 
-    name,
-    population,
-    area 
+SELECT
+name,
+population,
+area
 FROM World
-WHERE area >= 3000000 OR  population >= 25000000;
+WHERE population >= 25000000 OR area >= 3000000;
